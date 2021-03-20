bcf5b3b6c4c19114f90dae1e4ce6716f5da86376
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-20 15:33:33 +0530

    Swapnil : Added basic variable creation,assigning value and type of variables

---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+var iNo = 10; //    combination of alphabates
+var iNo2 = 10.5; // combination of alphabates and digits
+var _name = "Swapnil Ramesh Adhav"; //  combination of _ and alphabates
+var $address = 'Pune'; //   combination of $ and alphabates
+
+console.log(iNo);   //  Printing the value stored in iNo
+console.log(iNo2);  //  Printing the value stored in iNo2
+console.log(_name); //  Printing the value stored in _name
+console.log($address);  //  Printing the value stored in $address
+
+console.log(typeof(iNo));   //  Printing the type of iNo
+console.log(typeof(iNo2));  //  Printing the type of iNo2
+console.log(typeof(_name)); //  Printing the type of _name
+console.log(typeof($address));  //  Printing the type of $address
