03be00ce837b75cbcb2eb63ce2cc6d74383040ac
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-20 15:35:17 +0530

    Updated

--- basics/variable.js
+++ basics/variable.js
@@ -8,7 +8,7 @@ console.log(iNo2);  //  Printing the value stored in iNo2
 console.log(_name); //  Printing the value stored in _name
 console.log($address);  //  Printing the value stored in $address
 
-console.log(typeof(iNo));   //  Printing the type of iNo
-console.log(typeof(iNo2));  //  Printing the type of iNo2
-console.log(typeof(_name)); //  Printing the type of _name
-console.log(typeof($address));  //  Printing the type of $address
+console.log("type of iNo : "+typeof(iNo));   //  Printing the type of iNo
+console.log("type of iNo2 : "+typeof(iNo2));  //  Printing the type of iNo2
+console.log("type of _name : "+typeof(_name)); //  Printing the type of _name
+console.log("type of $address : "+typeof($address));  //  Printing the type of $address
