9ecb42b07277f4770f5d42a8f192c4e96060b2b2
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-21 12:30:55 +0530

    Added parseInt.js

---
+++ Numbers/parseInt.js
@@ -0,0 +1,13 @@
+// parseInt function takes string literal as input and return number.
+
+var str = "3.14";
+
+console.log("......Before parseInt......");
+console.log(str);
+console.log(typeof(str));
+
+str = parseInt(str);
+
+console.log("......After parseInt......");
+console.log(str);
+console.log(typeof(str));
