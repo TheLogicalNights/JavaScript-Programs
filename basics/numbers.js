4975b3afef77f7c9d535b30b0343d94264c0dd9c
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-21 12:20:21 +0530

    Added numbers.js

---
+++ basics/numbers.js
@@ -0,0 +1,23 @@
+var iNo1 = 15;
+var iNo2 = 5;
+
+//Addition
+
+var sum = iNo1+iNo2;
+
+//Substraction
+
+var sub = iNo1-iNo2;
+
+//Multiplication
+
+var mult = iNo1*iNo2;
+
+//Division
+
+var div = iNo1/iNo2;
+
+console.log("Addition is : "+sum);
+console.log("Substraction is : "+sub);
+console.log("Multiplication is : "+mult);
+console.log("Division is : "+div);
