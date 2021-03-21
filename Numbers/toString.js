85875497c861c312ee6da63eaf69cd543f06ba8e
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-21 12:25:04 +0530

    Added number method

---
+++ Numbers/toString.js
@@ -0,0 +1,13 @@
+// To string method converts a number into string....
+
+var iNo = 10;
+
+console.log("......Before toString......")
+console.log(iNo);
+console.log(typeof(iNo));
+
+iNo = iNo.toString();
+
+console.log("......After toString......")
+console.log(iNo);
+console.log(typeof(iNo));
