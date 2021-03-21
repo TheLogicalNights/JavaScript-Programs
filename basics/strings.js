7edb3d57648d091154578c8c160b4b7ceee9d8eb
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-21 12:56:22 +0530

    Added string.js

---
+++ basics/strings.js
@@ -0,0 +1,13 @@
+// JavaScript String contains double quoted as well as single quoted string literals.
+
+var doubleQuotedString = "Swapnil Ramesh Adhav";
+console.log(doubleQuotedString);
+
+var singleQuotedString = 'Swapnil Ramesh Adhav';
+console.log(singleQuotedString);
+
+var doubleQuotedString2 = "I love 'Programming'";
+console.log(doubleQuotedString2);
+
+var singleQuotedString2 = 'I love "Programming"';
+console.log(singleQuotedString2);
