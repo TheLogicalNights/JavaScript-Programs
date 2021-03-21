8331d8ced5ca73c49c05dac01435a181b7079b4b
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-21 12:36:12 +0530

    Added parseFloat method

---
+++ Numbers/parseFloat.js
@@ -0,0 +1,13 @@
+// parseFloat function takes string literal as input and return number.
+
+var str = "3.14";
+
+console.log("......Before parseFloat......")
+console.log(str);
+console.log(typeof(str));
+
+str = parseFloat(str);
+
+console.log("......After parseFloat......")
+console.log(str);
+console.log(typeof(str));
