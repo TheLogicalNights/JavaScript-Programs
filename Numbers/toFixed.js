d8af8b0ec4b3ed9d529bffa151e7fd576ca50dd8
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-21 12:42:28 +0530

    Added toFixed.js

---
+++ Numbers/toFixed.js
@@ -0,0 +1,17 @@
+// toFixed function takes floating number as input and rounds it off to given position
+
+var iNo = 87.356875;
+
+console.log("......Before toFixed......");
+console.log(iNo);
+
+iNo1 = iNo.toFixed();
+iNo2 = iNo.toFixed(2);
+iNo3 = iNo.toFixed(3);
+iNo4 = iNo.toFixed(4);
+
+console.log("......After toFixed......");
+console.log(iNo1);
+console.log(iNo2);
+console.log(iNo3);
+console.log(iNo4);
