7626b07975bf495c0c305dcb8c3d65e9cfb3f68f
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-22 17:07:54 +0530

    Arrow Functions

---
+++ ES6/arrow.js
@@ -0,0 +1,10 @@
+// Arrow functions allows a short syntax for writing function expressions.
+
+var iNo1 = 5;
+var iNo2 = 10;
+
+const sum = (iNo1,iNo2) =>{
+    return iNo1+iNo2;
+}
+
+console.log(sum(iNo1,iNo2));
