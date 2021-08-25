49789b9497ff43bcc325aaee77a1cabf8b140de5
Swapnil Adhav <swapniladhav560@gmail.com>
2021-08-25 11:48:50 +0530

    Added program 2

---
+++ Assignment-1/Program-2/main.js
@@ -0,0 +1,11 @@
+// Problem Statement : Program to print 'Jay Ganesh' 5 times on the screen
+
+function display()
+{
+    for(var i=0;i<5;i++)
+    {
+        console.log("Jay Ganesh");
+    }
+}
+
+display();
